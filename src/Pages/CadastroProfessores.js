4a18d7c77a21b9ee1d2ca267eb1ad26dc6251c81
HoCH-DS <caio_hoch@otlook.com>
2022-09-13 17:31:35 -0300

    Cadastro de Turmas / Cadastro de Professores

--- src/Pages/CadastroProfessores.js
+++ src/Pages/CadastroProfessores.js
@@ -5,6 +5,8 @@ import styles from "../Styles/CadastroAlunos/Prof.module.css"
 import logo from "../IMG/Logo WMS.png"
 import { Foto } from "../Components/Foto"
 
+import api from '../Services/api'
+
 export default function CadastroProfessores() {
     return (
         <div className={styles.container}>
@@ -24,23 +26,40 @@ export default function CadastroProfessores() {
 
             <div className={styles.formContainer}>
                 <h1 className={styles.h1}>Cadastro de Professor(a)</h1>
-                <form>
+                <form onSubmit={CadastrarProf}>
                     <label className={styles.label}>Nome</label>
                     <br />
-                    <Input type="text" placeholder="Digite o seu Nome" name="nome" />
+                    <Input id="nome" type="text" placeholder="Digite o seu Nome" name="nome" />
                     <br />
                     <label className={styles.label}>NIF</label>
                     <br />
-                    <Input type="number" name="nif" placeholder="Digite seu NIF" />
+                    <Input id="nif" type="number" name="nif" placeholder="Digite seu NIF" />
                     <br />
                     <label className={styles.label}>Senha</label>
                     <br />
-                    <Input type="password" name="senha" placeholder="Digite sua Senha" />
+                    <Input id="senha" type="password" name="senha" placeholder="Digite sua Senha" />
                     <br />
                     <Button>Cadastrar</Button>
                 </form>
             </div>
         </div>
     )
+
+}
+
+function CadastrarProf(event){
+    event.preventDefault()
+
+    const nomeProf = document.getElementById('nome').value
+    const nif = document.getElementById('nif').value
+    const senha = document.getElementById('senha').value
+
+    const body = {'nome':nomeProf, 'nif': nif, 'senha':senha };
+
+    console.log(body)
+
+    api.post(
+        "api/professor/save",body
+    );
 
 }
