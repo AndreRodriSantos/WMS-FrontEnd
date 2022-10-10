c521372045d5dd3e88772f8a99203ac771c3e40c
Andre Rodrigues <andrerodrisantos15@gmail.com>
2022-10-10 11:18:35 -0300

    Git de Correção

--- src/Pages/CadastroAlunos.js
+++ src/Pages/CadastroAlunos.js
@@ -49,11 +49,7 @@ export default function CadastroAlunos() {
                     <br/>
                     <Input label="N° Matricula" id="numMatricula" type="number" onChange={(e) => setMatricula(e.target.value)} name="numMatricula" placeholder="Digite o N° Matrícula" />
                     <br/>
-<<<<<<< HEAD
-                    <Input label="Email" id="email" type="email" onChange={(e) => setEmail(e.target.value)} name="email" placeholder="Digite seu Email" />
-=======
                     <Input label="Email" id="emailAluno" type="email" onChange={(e) => setEmailAluno(e.target.value)} name="emailAluno" placeholder="Digite o Email" />
->>>>>>> BranchCaio
                     <br/>
                     <InputSenha label="Senha" id="senha"  id_eye="eye"  type="password" onChange={(e) => setSenha(e.target.value)} name="senha" placeholder="Digite sua Senha" />
                     <br/>
