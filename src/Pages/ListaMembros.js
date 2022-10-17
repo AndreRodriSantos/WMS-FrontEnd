ca648899448974bf8def3c720e81d80a96329e7d
Andre Rodrigues <andrerodrisantos15@gmail.com>
2022-10-17 11:13:36 -0300

    Melhorias na Lista Membros

--- src/Pages/ListaMembros.js
+++ src/Pages/ListaMembros.js
@@ -10,85 +10,13 @@ import LinhaMembros from "../Components/Membros/LinhaMembro";
 export default function ListaMembros() {
 
     const [alunos, setAlunos] = useState([])
-    const [professores, setProfessores] = useState([])
-    let [membrosCheck, setMembrosCheck] = useState([])
-
-    const [alunosAdd, setAlunosAdd] = useState([])
-    const [professoresAdd, setProfessoresAdd] = useState([])
-
-    const [membroTurma , setMembroTurma] = useState([])
-
-    //Juntando dois json em uma Variavel
-    //var membroList = alunos.concat(professores)
-    //var membroAdd = alunosAdd.concat(professoresAdd)
-
-    const [text, setText] = useState('')
+    const [membrosCheck, setMembrosCheck] = useState([])
+    const [pesquisa, setPesquisa] = useState([])
+    const [membrosTurma, setMembrosTurma] = useState([])
     const [list, setList] = useState([])
-    //console.log(list)
-
-    function getTurma(id){
-       return api.get(`api/turma/${id}`).then(response => response.data)
-    }
-
-    //Fazendo Busca pelo Membro
-    useEffect(() => {
-        if (text === '') {
-            setList(alunos)
-        } else {
-            setList(
-                alunos.filter((item) =>
-                    item.nome.toLowerCase().indexOf(text.toLowerCase()) > -1                   
-                )
-            );
-        }
-    }, [text]);
-
-    //Ordernando busca de A & Z
-    const OrderTitle = () => {
-        let newList = [...alunos]
-
-        newList.sort((a, b) => (a.nome > b.nome ? 1 : b.nome > a.nome ? -1 : 0))
-
-        setList(newList)
-    }
-
-    function getAluno() {
-        return api.get("api/aluno/list").then(
-            response => {
-                const alu = response.data
-
-                alu.map(a => {
-                    if(a.turma == null){
-                        setAlunos(alunos => [...alunos, a])
-                    }else if (a.turma.id == localStorage.getItem("idTurma")){
-                        setAlunosAdd(alunosAdd => [...alunosAdd, a])
-                    }
-                })
-            }
-        )
-    }
-    
-
-    function getMembros() { 
-        return api.get(`api/membros/teste/${localStorage.getItem("idTurma")}`).then(
-            response => {
-                const membro = response.data
-                console.log(membro);
-                membro.map(m => {
-                    setMembroTurma(membroTurma => [...membroTurma, m])
-                })         
-            }
-        )
-    }
-
-    useEffect(() => {
-        getAluno()
-        getMembros()
-        
-    }, [])
-
 
     function AbrirList() {
+       
         const btnAddMembro = document.getElementById('btnAddMembro')
         const pesquisa = document.getElementById('pesquisa')
         const list = document.getElementById('listMembros')
@@ -98,54 +26,109 @@ export default function ListaMembros() {
         list.style.left = "0"
 
         setTimeout(() => {
+            list.style.maxHeight = "100%"
             setList(alunos)
         }, 500);
-        
     }
+
+    async function AdicionarList() {
+        const turma = await getTurma(localStorage.getItem("idTurma"))
+
+        membrosCheck.map((m) => {
+            const body = { turma, "aluno": m };
+
+            api.post(`api/membros/save`, body)
+        })
+
+        if (membrosCheck.length != 0) {
+            window.location.reload()
+        }
+    }
+
+    const OrdenarList = () => {
+        var newList = [...alunos]
+        newList.sort((a, b) => (a.nome > b.nome ? 1 : b.nome > a.nome ? -1 : 0))
+        setList(newList)
+    }
+
+    useEffect(() => {
+        if (pesquisa === '') {
+            setList(alunos)
+        } else {
+            setList(
+                alunos.filter((item) =>
+                    item.nome.toLowerCase().indexOf(pesquisa.toLowerCase()) > -1
+                )
+            )
+        }
+    }, [pesquisa]);
 
     function onCheck(membro) {
         setMembrosCheck(membrosCheck => [...membrosCheck, membro])
-        /* console.log(membrosCheck); */
     }
 
     function offCheck(membro) {
+        membrosCheck.map((m, index) => {
 
-    }
-
-    async function addList() {
-
-        const turma = await getTurma(localStorage.getItem("idTurma"))
-
-        membrosCheck.map((m) => { 
-            const body =  {turma , "aluno" : m}; 
-            /* console.log(body); */
-            
-            api.post(`api/membros/save`, body)
-
+            if (m.id == membro.id) {
+                membrosCheck.splice(index, 1)
+            }
         })
     }
 
-    function tirarAluno(id) { 
-        api.delete(`api/membros/${id}`) 
+    function tirarAluno(id) {
+        api.delete(`api/membros/${id}`)
+        window.location.reload()
     }
+
+    function getTurma(id) {
+        return api.get(`api/turma/${id}`).then(response => response.data)
+    }
+
+    async function getAlunos() {
+        return api.get("api/aluno/list").then(
+            response => {
+                const listAlunos = response.data
+
+                listAlunos.map((a) => {
+
+                    setAlunos(alunos => [...alunos, a])
+                })
+            },
+        )
+
+    }
+
+    async function getMembros() {
+        const turma = await getTurma(localStorage.getItem("idTurma"))
+        const membros = turma.membros
+        membros.map(m => {
+            setMembrosTurma(membrosTurma => [...membrosTurma, m])
+        })
+    }
+
+    useEffect(() => {
+        getMembros()
+        getAlunos()
+    }, [])
 
     return (
         <section className={styles.container}>
             <div className={styles.AddMembros}>
                 <div id='btnAddMembro' onClick={AbrirList} className={styles.baseAddMembros}>
-                    <span onClick={addList} className={styles.button}>
+                    <span onClick={AdicionarList} className={styles.button}>
                         <i className="fa-regular fa-plus"></i>
                     </span>
                     <div id='pesquisa' className={styles.pesquisa}>
-                        <span onClick={OrderTitle} className={styles.btnOrderTitle}>
+                        <span onClick={OrdenarList} className={styles.btnOrderTitle}>
                             <i className="fa-solid fa-arrow-up-a-z"></i>
                         </span>
-                        <SearchInput id='pesquisa' placeholder="Pesquise um pessoa" value={text} onChange={(search) => setText(search)} />
+                        <SearchInput id='pesquisa' placeholder="Pesquise um pessoa" value={pesquisa} onChange={(search) => setPesquisa(search)} />
                     </div>
                 </div>
                 <ul id="listMembros" className={styles.listPesquisa}>
                     {
-                    list.length == 0 ? <li>Sem resultados <i className="fa-regular fa-face-sad-tear"></i></li> : list.map((m) => <LinhaPesquisa key={m.nif == undefined ? m.codMatricula + m.id : m.nif + m.id } membro={m}  onCheck={onCheck} offCheck={offCheck} />)
+                        list.length == 0 ? <li>Sem resultados <i className="fa-regular fa-face-sad-tear"></i></li> : list.map((m) => <LinhaPesquisa key={m.id} membro={m} onCheck={onCheck} offCheck={offCheck} />)
                     }
                 </ul>
 
@@ -168,7 +151,7 @@ export default function ListaMembros() {
                             </thead>
                             <tbody id="lista" className={styles.body}>
                                 {
-                                    membroTurma.map((m) => <LinhaMembros key={m.id} funcao={m.nif == undefined ? "ALUNO"  :  "PROFESSOR"} aluno={m.aluno} membro={m} tirarAluno={tirarAluno}/>)                               
+                                    membrosTurma.map((m) => <LinhaMembros key={m.id} funcao={m.nif == undefined ? "ALUNO" : "PROFESSOR"} aluno={m.aluno} membro={m} tirarAluno={tirarAluno} />)
                                 }
                             </tbody>
                         </table>
